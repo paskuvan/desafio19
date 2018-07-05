97a66191e387fdc731d9e209dff046e61e572715
paskuvan <majopaskuvan@gmail.com>
2018-07-05 14:28:46 -0400

    reload

--- ex7.rb
+++
@@ -1,9 +0,0 @@
-#config.ru
-require 'rack'
-
-class MiPrimeraWebApp
- def call(env)
-  [200, {'Content-Type' => 'text/html'}, ['<p>hola hola hola hola hola hola hola hola hola hola </p>']]
- end
-end
-run MiPrimeraWebApp.new
